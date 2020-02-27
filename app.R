28906431bee1df66a0cede8377360431f373e745
Nick Meuli <nick@harmonic.co.nz>
2020-02-27 17:32:57 +1300

    audio begins, fix wolf color

--- app.R
+++ app.R
@@ -9,6 +9,8 @@ library(shinydashboard)
 library(highcharter)
 library(DT)
 library(dplyr)
+library(stringr)
+
 #source("imageBoxFunctions.R", local = TRUE)
 
 if (file.exists('data/dont_commit/char_stats.rds')) {
@@ -29,13 +31,14 @@ ui<- dashboardPage(
                 choices = setdiff(all_plot_vars, 'tier'),
                 selected = 'fall_speed'),
     prettyCheckbox('img_markers', label = 'Images as markers?', shape = 'curve'),
-    prettyCheckbox('tier_boxplot', label = "Group by tier?", shape = 'curve'),
+    prettyCheckbox('use_tiers_data', label = "Group by tier?", shape = 'curve'),
     switchInput(
       inputId = "tier_color_scheme",
       onLabel = 'dumpster \nto glory',
       offLabel = 'boring',
       label = "<i class=\"fa fa-grin-hearts\"></i>"
-    )
+    ),
+    prettyCheckbox('playsound', label = 'SOUND')
   ), 
   dashboardBody(
     #titlePanel('SUPER STATS BROS'),
@@ -55,35 +58,37 @@ ui<- dashboardPage(
 
 server <- function(input, output, session) {
   
-  observeEvent(input$tier_boxplot, {
+  observeEvent(input$use_tiers_data, {
     toggle('tier_color_scheme')
-   
-    tableProxy %>%
-      selectRows('')
+    
+    # input$main_plot_click
   })
   
-  
+  # click_data <- reactive({
+  #   input$main_plot_click
+  # }) 
+  # 
   reactive_dataset <- reactive({
     
-    if(input$tier_boxplot){
+    if(input$use_tiers_data){
       tier_stats %>% 
-             select(name = tier, yvar = !!paste0(input$yvar_input, '_mean'), yvar_sd = !!paste0(input$yvar_input, '_sd'),
-                    xvar = !!paste0(input$xvar_input, '_mean'), xvar_sd = !!paste0(input$xvar_input, '_sd')) %>% 
-             arrange(name) %>% 
-             mutate(color = if(input$tier_color_scheme) {tier_cols_dg} else {tier_cols_bg}, image = tier_images) %>%
-             do(., if(input$img_markers) {mutate(., marker = purrr::map(image, ~ list(symbol = sprintf("url(%s)", .x), width = 32, height = 32)))} else {.}) 
-        
-        } else {
+        select(name = tier, yvar = !!paste0(input$yvar_input, '_mean'), yvar_sd = !!paste0(input$yvar_input, '_sd'),
+               xvar = !!paste0(input$xvar_input, '_mean'), xvar_sd = !!paste0(input$xvar_input, '_sd')) %>% 
+        arrange(name) %>% 
+        mutate(color = if(input$tier_color_scheme) {tier_cols_dg} else {tier_cols_bg}, image = tier_images) %>%
+        do(., if(input$img_markers) {mutate(., marker = purrr::map(image, ~ list(symbol = sprintf("url(%s)", .x), width = 32, height = 32)))} else {.}) 
+      
+    } else {
       char_stats %>%
-      select(name = character, image = icon_url_path, color, xvar = input$xvar_input, yvar = input$yvar_input) %>%  
-             #group := ifelse(input$tier_boxplot, !!'tier', one_of('you_get_nothing_you_lose'))) %>%
-      filter_all(all_vars(!is.na(.))) %>%
-      mutate(color = toupper(color)) %>%
-      do(., if(input$img_markers) {mutate(., marker = purrr::map(image, ~ list(symbol = sprintf("url(%s)", .x), width = 32, height = 32)))} else {.}) %>% 
-      arrange(name)
-      }
+        select(name = character, image = icon_url_path, color, xvar = input$xvar_input, yvar = input$yvar_input) %>%  
+        #group := ifelse(input$use_tiers_data, !!'tier', one_of('you_get_nothing_you_lose'))) %>%
+        filter_all(all_vars(!is.na(.))) %>%
+        mutate(color = if(input$img_markers) {sapply(color, function(x) paste0('rgba(', str_flatten(as.vector(col2rgb(x)), ','), ',0.3)')) } else {toupper(color)}) %>%
+        do(., if(input$img_markers) {mutate(., marker = purrr::map(image, ~ list(symbol = sprintf("url(%s)", .x), width = 32, height = 32)))} else {.}) %>% 
+        arrange(name)
+    }
     
-    
+    # color = purrr::map(color, ~paste0('rgba(', str_flatten(as.vector(col2rgb(.x)), ','), ',0.3)')))
   })
   
   main_hc <- reactive({
@@ -96,19 +101,35 @@ server <- function(input, output, session) {
       hc_chart(zoomType = 'xy') %>% 
       hc_add_event_point(event = "click") %>%
       #hc_add_event_point(event = "unselect") %>%
-      hc_add_series(data = if (is.null(input$main_datatable_rows_selected) | input$tier_boxplot) {NULL}
-                    else {select(reactive_dataset()[input$main_datatable_rows_selected, ], x= xvar, y = yvar, color, name, image)},
-                    type = 'scatter', marker = list(radius = 30, lineColor = '#32CD32', lineWidth = 5)) %>%
       hc_tooltip(useHTML = TRUE,
                  formatter= JS("function() {return (this.point.name + '<span style=\"color: this.point.color\"></br>y: <b>' + this.point.y + '</b> </br> x: <b>' +
    this.point.x + '</b></span></br><img src=\"'+ this.point.image + '\"width=\"48\" height=\"48\"/></br>');}")) %>%
       hc_add_theme(hc_theme_chalk())
-    #slice(reactive_dataset(), 0)
   })
   
   output$main_plot <- renderHighchart({
-    # browser()
-    main_hc()
+    #browser()
+    #str_flatten(as.vector(col2rgb(reactive_dataset()$color)), ','), ',0.3)')
+    main_hc() %>% 
+      hc_add_series(data = if (is.null(input$main_datatable_rows_selected) | input$use_tiers_data) {NULL}
+                    else {select(reactive_dataset()[input$main_datatable_rows_selected, ], x= xvar, y = yvar, color, name, image)},
+                    type = 'scatter', marker = list(radius = 30, lineWidth = 5, lineColor = ifelse(input$img_markers, "rgba(32,205,32, 0.3)", 'rgba(32,205,32, 1)')
+                    ))
+  })
+  
+  observeEvent(input$playsound, {
+    if (input$playsound) {
+      insertUI(selector = "#playsound",
+               where = "afterEnd",
+               ui = tags$audio(src = "audio/lalalalalalaaa.mp3", type = "audio/mp3", autoplay = NA, controls = NA, style="display:none;")
+      ) 
+    } else {
+      insertUI(selector = "#playsound",
+               where = "afterEnd",
+               ui = tags$audio(src = "audio/hahaha.mp3", type = "audio/mp3", autoplay = NA, controls = NA, style="display:none;")
+      )
+    }
+    
   })
   
   output$main_datatable <- renderDataTable({
@@ -117,16 +138,35 @@ server <- function(input, output, session) {
   
   tableProxy <-  dataTableProxy("main_datatable")
   
-  # observeEvent(input$main_plot_unselect, {
-  # browser()
-  #   })
+   observeEvent(input$main_datatable_rows_selected, {
+     sound_clip =  str_subset(list.files('www/audio/announcer/'), reactive_dataset()[input$main_datatable_rows_selected,]$name )
+     
+     if (length(sound_clip) == 1) {
+       insertUI(selector = "#playsound",
+                where = "afterEnd",
+                ui = tags$audio(src = paste0('audio/announcer/', sound_clip), type = paste0("audio/", str_extract(sound_clip, '\\..+$')), 
+                                autoplay = NA, controls = NA, style="display:none;"))
+     }
+     })
   
   observeEvent(input$main_plot_click, {
     if (input$main_plot_click$series == 'Series 1') {
       charId = which(reactive_dataset()$name == input$main_plot_click$name)
+      
+      sound_clip =  str_subset(list.files('www/audio/announcer/'), reactive_dataset()[charId,]$name )
+      
+      if (length(sound_clip) == 1) {
+      insertUI(selector = "#playsound",
+               where = "afterEnd",
+               ui = tags$audio(src = paste0('audio/announcer/', sound_clip), type = paste0("audio/", str_extract(sound_clip, '\\..+$')), 
+                               autoplay = NA, controls = NA, style="display:none;"))
+      }
+      
       tableProxy %>% 
         selectRows(charId) %>%
         selectPage( (charId-1) %/% number_of_rows_dt + 1)
+      
+      
     } else {
       #browser()
       tableProxy %>%
--- load/get_colors.R
+++ load/get_colors.R
@@ -9,4 +9,6 @@ background_colors = list()
 for (name in char_list$smash_css_names) {
   background_colors[[name]] <-  read_html(paste0('https://www.smashbros.com/assets_v2/css/fighter/',name,'.css')) %>% html_text() %>% str_replace('.*\\.fighter-bg\\{background:([^\n\\}]*).*', '\\1') %>% str_remove('\n')
 }
-background_colors = stack(background_colors) %>% rename(character = ind, color = values) %>% mutate(character = str_replace_all(character,  setNames(as.character(char_list$app_names), paste0('^', char_list$smash_css_names, '$'))))
+background_colors = stack(background_colors) %>% rename(character = ind, color = values) %>% 
+  mutate(character = str_replace_all(character,  setNames(as.character(char_list$app_names), paste0('^', char_list$smash_css_names, '$'))), 
+         color = str_replace_all(color, "#(.)\\1{2}$", str_flatten(c('#', rep('\\1', 6))))) # turn color codes like #ccc into #cccccc
